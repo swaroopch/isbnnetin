3a08234c46b98e7a7731c05c493a16e9da6ac756
Swaroop C H <swaroop@swaroopch.com>
2010-12-21 00:52:02 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Isbnnetin::Application.config.secret_token = '59e3a861da5e89af38f6bb8bff11f79cbf7cbcd6f3abeb65bcd704e5a2bfa6ada4e550127e6defb926a186151b4bf5b6fbaa83b1aa9d3207e7a66e0c34c5d826'
