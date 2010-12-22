3760ff3b561006f98896a4b200936c8a8536bb8c
Swaroop C H <swaroop@swaroopch.com>
2010-12-22 07:12:25 +0530

    Renaming config file for ease of human reference

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -2,7 +2,7 @@
 
 require 'configatron'
 
-yaml_config = YAML.load_file("#{Rails.root}/config/config.yml")
+yaml_config = YAML.load_file("#{Rails.root}/config/app_config.yml")
 
 configatron.configure_from_hash(yaml_config['common'])
 configatron.configure_from_hash(yaml_config[Rails.env])
