929dd0eacfbb8a05bf4f539a747f4e309d6100a6
Swaroop C H <swaroop@swaroopch.com>
2010-12-25 00:52:43 +0530

    Fetching the relevant info

--- app/controllers/book_controller.rb
+++ app/controllers/book_controller.rb
@@ -3,15 +3,36 @@ class BookController < ApplicationController
   end
 
   def view
-    @isbn = params[:isbn]
-    if Bookprice::is_isbn(@isbn)
-      @stores = Rails.cache.fetch(@isbn, :expires_in => 1.day) do
-        Bookprice::prices(@isbn)
-      end
-      @not_available = Bookprice::NOT_AVAILABLE
-    else
-      render :text => '404 Not Found', :status => 404
+    @isbn = canonicalize_isbn(params[:isbn])
+    if @isbn.nil? || !is_isbn(@isbn)
+      render :text => 'Not Found', :status => 404
+      return
     end
+
+    @bookinfo = Rails.cache.fetch("amazon_info:#{@isbn}", :expires_in => 1.day) { AmazonInfo::book_info(@isbn) }
+    if @bookinfo.nil?
+      @bookinfo = Rails.cache.fetch("flipkart_info:#{@isbn}", :expires_in => 1.day) { FlipkartInfo::book_info(@isbn) }
+    end
+
+    unless @bookinfo.nil?
+      @bookseer = BookseerInfo::link(@bookinfo)
+    end
+
+    @stores = Rails.cache.fetch("prices:#{@isbn}", :expires_in => 1.day) { Bookprice::prices(@isbn) }
+    @not_available = Bookprice::NOT_AVAILABLE
+  end
+
+
+  private
+
+  def canonicalize_isbn(text)
+    unless text.nil?
+      text.to_s.gsub('-', '').upcase
+    end
+  end
+
+  def is_isbn(text)
+    /^[0-9]{9}[0-9xx]$/.match(text) or /^[0-9]{13}$/.match(text)
   end
 
 end
